16e0ab00b390eeb03d6729318eed2b601efa0287
codingcreedd <moubayedmarwancoding@gmail.com>
2024-01-11 21:23:49 +0200

    delete unused js file

--- lp.js
+++
@@ -1,8 +0,0 @@
-const langsInfo = document.querySelectorAll('.langs-info');
-
-
-langsInfo.forEach(lang => {
-    lang.addEventListener('click', e => {
-        gsap.to(e.target, {y: 5, rotate: 720, repeat:1, yoyo: true,ease:"power1", duration: 0.5})
-    });
-});
